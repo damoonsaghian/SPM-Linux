3c29b74cc29dc90ab1ea2b19d498c687065366da
Damoon Saghian <damoonsaghian84@gmail.com>
2024-07-10 14:53:30 +0330

    Add files via upload

---
+++ packages/spmbuild.sh
@@ -0,0 +1,48 @@
+apt-get -qq install sway swayidle xwayland lua5.3 lua-lgi gir1.2-gtk-4.0 gnome-console
+cp /mnt/os/{sway.conf,swapps.py} /usr/local/share/
+# the following is to prevent normal users from changing Sway's config
+# this means that, swayidle can't be disabled by a normal user (see sway.conf)
+echo -n '# run sway (if this script is not called by root or a display manager, and this is the first tty)
+if [ ! "$(id -u)" = 0 ] && [ -z "$DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
+	[ -f "$HOME/.profile" ] && . "$HOME/.profile"
+	exec sway -c /usr/local/share/sway.conf
+fi
+' > /etc/profile.d/zz-sway.sh
+
+# mono'space fonts:
+# , wide characters are forced to squeeze
+# , narrow characters are forced to stretch
+# , bold characters don’t have enough room
+# proportional font for code:
+# , generous spacing
+# , large punctuation
+# , and easily distinguishable characters
+# , while allowing each character to take up the space that it needs
+# "https://github.com/iaolo/iA-Fonts/tree/master/iA%20Writer%20Quattro"
+# "https://input.djr.com/"
+apt-get -qq install fonts-noto-core fonts-hack
+mkdir -p /etc/fonts
+echo -n '<?xml version="1.0"?>
+<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+<fontconfig>
+	<selectfont>
+		<rejectfont>
+			<pattern><patelt name="family"><string>NotoNastaliqUrdu</string></patelt></pattern>
+			<pattern><patelt name="family"><string>NotoKufiArabic</string></patelt></pattern>
+			<pattern><patelt name="family"><string>NotoNaskhArabic</string></patelt></pattern>
+		</rejectfont>
+	</selectfont>
+	<alias>
+		<family>serif</family>
+		<prefer><family>NotoSerif</family></prefer>
+	</alias>
+	<alias>
+		<family>sans</family>
+		<prefer><family>NotoSans</family></prefer>
+	</alias>
+	<alias>
+		<family>monospace</family>
+		<prefer><family>Hack</family></prefer>
+	</alias>
+</fontconfig>
+' > /etc/fonts/local.conf
