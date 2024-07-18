314dcd7a342b3058026ae72bd869aa16bbdcd0d5
Damoon Saghian <damoonsaghian84@gmail.com>
2024-07-18 12:33:01 +0330

    Add files via upload

--- packages/busybox/spmbuild.sh
+++ packages/busybox/spmbuild.sh
@@ -1,1 +1,6 @@
+project_dir="$(dirname "$0")"
+
 # git://busybox.net/busybox/tag/?h=1_36_1
+
+cp "$project_dir"/system.sh "$project_dir"/.cache/spm/bin/system
+chmod +x "$project_dir"/.cache/spm/bin/system
