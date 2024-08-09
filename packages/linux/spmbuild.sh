716ae84f97e1e0e7321bec0a2ded14f059e6be82
Damoon Saghian <damoonsaghian84@gmail.com>
2024-08-09 23:05:46 +0330

    Add files via upload

--- packages/linux/spmbuild.sh
+++ packages/linux/spmbuild.sh
@@ -2,3 +2,9 @@
 
 # https://firasuke.github.io/DOTSLASHLINUX/post/booting-the-linux-kernel-without-an-initrd-initramfs/
 # https://gitlab.alpinelinux.org/alpine/mkinitfs
+
+# no initramfs
+# , compile linux with kernel modules for sata, ysb, ...
+# , kernel parameter: root=PartUUID=...
+
+# custom init path, using kernel parameter: init=/spm/busybox/init
