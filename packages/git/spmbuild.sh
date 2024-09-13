f38d43615e7c730ac11d6d548c9e8efefd0786fd
Damoon Saghian <damoonsaghian84@gmail.com>
2024-09-13 23:42:43 +0330

    Add files via upload

---
+++ packages/git/spmbuild.sh
@@ -0,0 +1,4 @@
+# https://github.com/git/git/blob/master/INSTALL
+# https://github.com/git/git/blob/master/Makefile
+# configure git to use lsh instead of openssh:
+# 	https://github.com/git/git/blob/master/Documentation/config/ssh.txt
