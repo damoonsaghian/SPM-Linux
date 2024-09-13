23a89a5edc0ce759ffdc30f2eae67a3bcff145d7
Damoon Saghian <damoonsaghian84@gmail.com>
2024-09-13 23:44:49 +0330

    Add files via upload

--- packages/gnunet/spmbuild.sh
+++ packages/gnunet/spmbuild.sh
@@ -1,11 +1,3 @@
-# https://www.gnunet.org/en/use.html
-# https://docs.gnunet.org/
-# https://wiki.archlinux.org/title/GNUnet
-# https://manpages.debian.org/unstable/gnunet/
-# https://manpages.debian.org/unstable/gnunet/gnunet.conf.5.en.html
-# https://manpages.debian.org/unstable/gnunet/gnunet-directory.1.en.html
-# https://manpages.debian.org/unstable/gnunet/gnunet-fs.1.en.html
-
 # gnunet.conf:
 # WLAN_QUOTA_IN = unlimited
 # WLAN_QUOTA_OUT = unlimited
