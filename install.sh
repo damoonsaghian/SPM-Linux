b40efd409265e526a1874881c35f7357a64e934d
Damoon Saghian <damoonsaghian84@gmail.com>
2024-09-27 08:47:46 +0330

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,16 +1,24 @@
 set -e
 
-arch="$1"
+if [ "$1" = build ]; then
+	build_from_src=true
+	arch="$2"
+else
+	arch="$1"
+fi
+[ -z "$arch" ] && arch="$(uname --machine)"
 
-echo "available storage devices:"
+project_dir="$(dirname "$0")"
+gnunet_namespace=
+
+echo; echo "available storage devices:"
 ls -1 --color=never /sys/block | grep -v "^loop" | while read -r device_name; do
 	device_size="$(cat /sys/block/"$device_name"/size)"
 	device_size="$((device_size / 1000000))GB"
 	device_model="$(cat /sys/block/"$device_name"/device/model)"
 	printf "\t$device_name\t$device_size\t$device_model\n"
 done
-
-printf "enter the name of the device (the first word) to install the system on: "
+printf "enter the name of the device (the first word) to install SPM Linux on it: "
 read -r target_device
 
 root_partition="$(df / | tail -1 | cut -d " " -f 1 | cut -d / -f 3)"
@@ -68,40 +76,25 @@ then
 	mkfs.btrfs -f --quiet "$target_partition2" > /dev/null 2>&1
 fi
 
-mount "$target_partition2" /mnt
-# create a directory in /tmp and mount the root
-# create these directories:
-# apps spm dev proc sys tmp
-# copy builtin packages to $mount_dir/spm/packages
-# add $mount_dir/apps/bb and $mount_dir/apps to the begining of $PATH
+mkdir -p /tmp/spm-linux
+mount "$target_partition2" /tmp/spm-linux
+mkdir -p /tmp/spm-linux/{apps,spm,dev,proc,sys,tmp}
 
-# https://wiki.artixlinux.org/Main/Installation
-# https://gitea.artixlinux.org/artix
-# https://packages.artixlinux.org/
-#
-# https://docs.alpinelinux.org/user-handbook/0.1a/Installing/setup_alpine.html
-# https://gitlab.alpinelinux.org/alpine
-# https://gitlab.alpinelinux.org/alpine/alpine-conf
-# https://pkgs.alpinelinux.org/package/edge/main/x86_64/alpine-base
-#
-# https://kisslinux.org/ https://github.com/kisslinux/
-# https://www.linuxfromscratch.org/ https://www.linuxfromscratch.org/lfs/view/stable/
-# https://github.com/gobolinux
-# https://sta.li/
-# https://t2sde.org/handbook/html/index.html
-# https://buildroot.org/downloads/manual/manual.html
-
-mkdir -p /run/mount/spm-linux/spm/installed/system
-cp "$project_dir"/packages/system/spm.sh /run/mount/spm-linux/spm/installed/system/spm.sh
-if [ "$2" = "from-src" ]; then
-	echo "use_prebuilt = true" > /run/mount/spm-linux/spm/config
+mkdir -p /tmp/spm-linux/spm/installed/$gnunet_namespace/system
+cp "$project_dir"/packages/system/spm.sh /tmp/spm-linux/spm/installed/$gnunet_namespace/system/spm.sh
+if [ "$build_from_src" = true ]; then
+	echo "use_prebuilt = true" > /tmp/spm-linux/installed/$gnunet_namespace/system/spm.conf
 fi
 
-mkdir -p /run/mount/spm-linux/spm/downloads
-cp -r "$project_dir"/packages /run/mount/spm-linux/spm/downloads/"$my_name_space"
+echo "WLAN_QUOTA_IN = unlimited
+WLAN_QUOTA_OUT = unlimited
+WAN_QUOTA_IN = unlimited
+WAN_QUOTA_OUT = unlimited
+" > ~/.config/gnunet.conf
+gnunet-arm --restart
 
 ls -1 "$project_dir"/packages/ | while read -r pkg_name; do
-	url="gnunet://$my_name_space/packages/spm-linux/packages/$pkg_name"
+	url="gnunet://$gnunet_namespace/packages/spm-linux/packages/$pkg_name"
 	sh /run/mount/spm-linux/spm/installed/system/spm.sh install "$pkg_name" "$url"
 done
 
@@ -111,41 +104,24 @@ elif [ "$arch" = ppc64le ]; then
 	# create "syslinux.cfg" (only OPAL Petitboot based systems are supported)
 fi
 
-sh /run/mount/spm-linux/spm/installed/system/spm.sh install "gnunet://$my_name_space/packages/codev"
+sh /run/mount/spm-linux/spm/installed/system/spm.sh install "gnunet://$gnunet_namespace/packages/codev"
 
-echo; echo -n "set username: "
+echo; printf "set username: "
 read -r username
-groupadd -f netdev; groupadd -f bluetooth
-useradd --create-home --groups "$username",netdev,bluetooth,sudo --shell /bin/bash "$username" || true
-cat <<'__EOF__' >> "/home/$username/.bashrc"
-export PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> "
-shopt -q login_shell &&
-	printf '\nenter "system" to configure system settings\n'
-__EOF__
-
-while ! passwd --quiet "$username"; do
+/run/mount/spm-linux/apps/adduser --shell /bin/bash "$username"
+/run/mount/spm-linux/apps/addgroup --system sudo
+/run/mount/spm-linux/apps/adduser "$username" sudo
+while ! /run/mount/spm-linux/apps/passwd "$username"; do
 	echo "an error occured; please try again"
 done
 echo; echo "set sudo password"
-while ! passwd --quiet; do
+while ! /run/mount/spm-linux/apps/passwd; do
 	echo "an error occured; please try again"
 done
 # lock root account
-passwd --lock root
-
-echo; echo "setting timezone"
-# guess the timezone, but let the user to confirm it
-geoip_tz="$(wget -q -O- 'http://ip-api.com/line/?fields=timezone')"
-geoip_tz_continent="$(echo "$geoip_tz" | cut -d / -f1)"
-geoip_tz_city="$(echo "$geoip_tz" | cut -d / -f2)"
-tz_continent="$(ls -1 -d /usr/share/zoneinfo/*/ | cut -d / -f5 |
-	fzy -p "select a continent: " -q "$geoip_tz_continent")"
-tz_city="$(ls -1 /usr/share/zoneinfo/"$tz_continent"/* | cut -d / -f6 |
-	fzy -p "select a city: " -q "$geoip_tz_city")"
-# $HOME/.profile
-# TZ="/spm/installed/system/tzdata/${tz_continent}/${tz_city}"; export TZ
+/run/mount/spm-linux/apps/passwd --lock root
 
 echo; echo -n "installation completed successfully"
-printf "reboot the system? (Y/n)"
+printf "reboot the system? (y/N)"
 read -r answer
-[ "$answer" = n ] || reboot
+[ "$answer" = y ] && reboot
