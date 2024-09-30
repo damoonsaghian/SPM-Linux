ba08183ff7470988ee94aaebe9a2bcb664633137
Damoon Saghian <damoonsaghian84@gmail.com>
2024-09-30 14:15:47 +0330

    Add files via upload

---
+++ packages/spmbuild.sh
@@ -0,0 +1,4 @@
+# https://docs.voidlinux.org/config/session-management.html
+# https://git.sr.ht/~kennylevinsen/seatd
+# https://docs.voidlinux.org/config/power-management.html
+# https://pkgs.alpinelinux.org/package/edge/main/x86_64/acpid
