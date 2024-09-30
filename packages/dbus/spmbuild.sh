ee9b162c8ff92b840a499f53d6a4ec0ffa5a5f2d
Damoon Saghian <damoonsaghian84@gmail.com>
2024-09-30 10:36:46 +0330

    Add files via upload

--- packages/dbus/spmbuild.sh
+++ packages/dbus/spmbuild.sh
@@ -1,1 +1,11 @@
+# in system.conf:
+# <includedir>/apps/dbus-sys</includedir>
+# <servicedir>/apps/dbus-sys/services</servicedir>
+# in session.conf:
+# <includedir>/apps/dbus</includedir>
+# <servicedir>/apps/dbus/services</servicedir>
 
+# $dbus_dir/session.conf
+# $dbus_dir/session.d/
+# $dbus_dir/services/
+# $dbus_sys_dir for trusted packages
