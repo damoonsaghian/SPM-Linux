fd4e43a45225fb895055526e6cc6d45e330093ae
Damoon Saghian <damoonsaghian84@gmail.com>
2024-10-21 10:10:19 +0330

    Add files via upload

---
+++ packages/spmbuild.sh
@@ -0,0 +1,6 @@
+# https://github.com/eudev-project/eudev
+
+# skip eudev-hwids
+# https://pkgs.alpinelinux.org/package/edge/main/x86_64/eudev-hwids
+
+# https://pkgs.alpinelinux.org/package/edge/main/x86_64/eudev-netifnames
