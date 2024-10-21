822e7b4523abc47df009c91aed71727d76e13584
Damoon Saghian <damoonsaghian84@gmail.com>
2024-10-21 10:23:59 +0330

    Add files via upload

--- packages/linux/spmbuild.sh
+++ packages/linux/spmbuild.sh
@@ -12,6 +12,7 @@
 # https://gitlab.alpinelinux.org/alpine/mkinitfs
 # https://wiki.gentoo.org/wiki/Custom_Initramfs
 # https://www.linuxfromscratch.org/blfs/view/svn/postlfs/initramfs.html
+# https://git.busybox.net/busybox/plain/docs/mdev.txt
 #
 # if a CPU microcode is needed, prepend it to initramfs
 # https://docs.kernel.org/arch/x86/microcode.html
