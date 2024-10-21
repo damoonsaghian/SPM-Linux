9870845d69dde612e973dd9bc05d8f501efa1eb7
Damoon Saghian <damoonsaghian84@gmail.com>
2024-10-21 10:11:41 +0330

    Add files via upload

---
+++ packages/spmbuild.sh
@@ -0,0 +1,13 @@
+# modemmanager A-GPS
+# assisted data can be downloaded from SUPL servers: supl.google.com with 7276 or 7275 port
+# then injected into the GPS device
+# https://www.freedesktop.org/software/ModemManager/api/latest/gdbus-org.freedesktop.ModemManager1.Modem.Location.html#gdbus-method-org-freedesktop-ModemManager1-Modem-Location.InjectAssistanceData
+
+# use gpsd for standalone gps devices (without cellular)
+# https://gpsd.gitlab.io/gpsd/index.html
+# gspd does not support injecting assisted data, yet
+# actually i don't know if there is any standalone gps device with this ability
+
+# https://wiki.archlinux.org/title/GPS
+
+# also get help from available sensors to calculate location even when the gps signal is lost
