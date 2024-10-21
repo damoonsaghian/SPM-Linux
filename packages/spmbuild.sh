06a1c0af81742cd4b5305f13bee1e10aedadc21d
Damoon Saghian <damoonsaghian84@gmail.com>
2024-10-21 10:13:10 +0330

    Add files via upload

---
+++ packages/spmbuild.sh
@@ -0,0 +1,20 @@
+# https://gitlab.freedesktop.org/NetworkManager/NetworkManager
+# https://networkmanager.dev/docs/
+# https://wiki.archlinux.org/index.php/NetworkManager
+
+# networkmanager without polkit, and with gnutls (instead of nss)
+# https://www.linuxfromscratch.org/blfs/view/stable/basicnet/networkmanager.html
+
+# iwd
+
+# modemmanager
+
+# RP-PPPoE
+
+# vpn plugins
+
+# making bridges (eg for tethering or creating a router)
+# https://wiki.debian.org/NetworkConfiguration
+# https://wiki.alpinelinux.org/wiki/Configure_Networking
+# https://wiki.alpinelinux.org/wiki/Bridge#Using_pre-up/post-down
+# https://wiki.alpinelinux.org/wiki/How_to_setup_a_wireless_access_point
