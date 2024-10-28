ac7a127a3b79eac891894d5daf3f6fa6cb4002c7
Damoon Saghian <damoonsaghian84@gmail.com>
2024-10-28 21:56:48 +0330

    Add files via upload

--- packages/gstreamer/spmbuild.sh
+++ packages/gstreamer/spmbuild.sh
@@ -1,12 +1,14 @@
+gst-plugin-pipewire
+gst-plugins-good # mp4/matroska/webm containers, plus mp3 and vpx
+gst-plugins-ugly
+gst-plugins-bad # av1(aom-libs), h264(openh264), h265(libde265), and aac(fdk-aac)
+
 # https://pipewire.org/
 # https://wiki.alpinelinux.org/wiki/PipeWire
 # https://docs.voidlinux.org/config/media/pipewire.html
 # https://wiki.archlinux.org/title/PipeWire
-
-# don't build alsa, jack, v4l plugins
-
+# don't build alsa and jack plugins
 # gst-plugin
-
 mkdir -p etc/wireplumber/main.lua.d
 echo 'device_defaults.properties = {
 	["default-volume"] = 1.0,
