e8bee759477e6ba0271cf05b26504d9f4f09d892
Damoon Saghian <damoonsaghian84@gmail.com>
2024-10-28 21:58:35 +0330

    Delete packages/sway/swaycap.py

--- packages/sway/swaycap.py
+++
@@ -1,9 +0,0 @@
-#!python3
-
-# an item for screen capturing (screenshot and recording)
-# grim wl-clipboard
-# grim -o "$$HOME/.cache/screen.png" | wl-copy --type text/uri-list "file://$$HOME/.cache/screen.png"
-# the screencast will be saved at "~/.cache/screen.mp4"
-
-# glib application
-# when arg[1] is stop, stop recording
