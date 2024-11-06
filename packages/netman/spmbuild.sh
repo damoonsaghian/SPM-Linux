e7dd9af783130669311a9d0259a64cdeef236b02
Damoon Saghian <damoonsaghian84@gmail.com>
2024-11-06 10:19:05 +0330

    Add files via upload

--- packages/netman/spmbuild.sh
+++ packages/netman/spmbuild.sh
@@ -1,3 +1,5 @@
+project_dir="$(dirname "$(realpath "$0")")"
+
 # https://gitlab.freedesktop.org/NetworkManager/NetworkManager
 # https://networkmanager.dev/docs/
 # https://wiki.archlinux.org/index.php/NetworkManager
@@ -6,8 +8,19 @@
 # https://www.linuxfromscratch.org/blfs/view/stable/basicnet/networkmanager.html
 
 # iwd
+# iwd service
+# doas rule
 
-# modemmanager
+# modemmanager without polkit
+# https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/blob/main/data/org.freedesktop.ModemManager1.conf.polkit
+# https://gitlab.freedesktop.org/mobile-broadband/ModemManager/-/blob/main/data/org.freedesktop.ModemManager1.policy.in.in
+# https://gitlab.alpinelinux.org/alpine/aports/-/blob/master/community/modemmanager/modemmanager.rules
+# https://modemmanager.org/docs/
+# modemmanager service
+
+# in a networkmanager dispatcher set the timezone
+# https://www.freedesktop.org/software/ModemManager/api/latest/gdbus-org.freedesktop.ModemManager1.Modem.Time.html
+# tz set "$offset"
 
 # RP-PPPoE
 
@@ -18,3 +31,11 @@
 # https://wiki.alpinelinux.org/wiki/Configure_Networking
 # https://wiki.alpinelinux.org/wiki/Bridge#Using_pre-up/post-down
 # https://wiki.alpinelinux.org/wiki/How_to_setup_a_wireless_access_point
+
+mkdir -p "$project_dir"/.cache/spm/apps/system
+ln "$project_dir"/sysman-connections.sh "$project_dir"/.cache/spm/apps/system/connections
+
+echo '#!/bin/sh
+# update system timezone
+' > /etc/NetworkManager/dispatcher.d/09-dispatch-script
+chmod 755 /etc/NetworkManager/dispatcher.d/09-dispatch-script
