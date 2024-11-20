1631d73f69a0da055cc6609fadcef1e50eb52fe7
Damoon Saghian <damoonsaghian84@gmail.com>
2024-11-20 17:32:11 +0330

    Add files via upload

---
+++ pi/spmbuild.sh
@@ -0,0 +1,5 @@
+project_dir="$(dirname "$0")"
+
+cp "$project_dir"/.cache/compiled/"$arch"/sudo.sh "$project_dir"/.cache/spm/"$arch"/sv/sudo
+
+# console level keybinding: when "F5-8" is pressed, switch fo virtual terminal 8, if a programming is running there
