db5e28f3957beabac282e04fc2e4d25e6f5aabf1
Damoon Saghian <damoonsaghian84@gmail.com>
2024-11-20 17:19:38 +0330

    Add files via upload

---
+++ spmbuild.sh
@@ -0,0 +1,6 @@
+project_dir="$(dirname "$0")"
+
+# https://gitlab.com/craigbarnes/dte
+
+# https://gitlab.com/craigbarnes/dte/-/blob/master/docs/packaging.md
+# make ICONV_DISABLE=1 BUILTIN_SYNTAX_FILES='dte config ini sh'
