a8adac246cfd846dbf66d9f65c5779e35ba4175e
Damoon Saghian <damoonsaghian84@gmail.com>
2024-11-24 14:24:19 +0330

    Add files via upload

--- dbus/spmbuild.sh
+++ dbus/spmbuild.sh
@@ -1,18 +1,19 @@
+project_dir="$(dirname "$0")"
+
 # https://man.archlinux.org/listing/dbus
 
-# sv-sys/dbus
+# sv/dbus
 
 # in system.conf:
-# <includedir>/apps/dbus-sys</includedir>
-# <servicedir>/apps/dbus-sys/services</servicedir>
-# in session.conf:
 # <includedir>/apps/dbus</includedir>
 # <servicedir>/apps/dbus/services</servicedir>
+# in session.conf:
+# <includedir>/home/.spm/apps/dbus</includedir>
+# <servicedir>/home/.spm/apps/dbus/services</servicedir>
 
 # $dbus_dir/session.conf
 # $dbus_dir/session.d/
 # $dbus_dir/services/
-# $dbus_sys_dir for trusted packages
 
 # https://github.com/chimera-linux/dbus-wait-for
 # https://git.sr.ht/~whynothugo/dbus-waiter
