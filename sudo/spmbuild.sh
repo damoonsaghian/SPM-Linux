3a2f955216568c422933c3910d64ce8003602d84
Damoon Saghian <damoonsaghian84@gmail.com>
2024-11-25 21:17:18 +0330

    Add files via upload

--- sudo/spmbuild.sh
+++ sudo/spmbuild.sh
@@ -1,6 +1,6 @@
 project_dir="$(dirname "$0")"
 
-# exas service: listen to dbus messages
-# exas client: send dbus messages
+# sudo service: listen to dbus messages
+# sudo client: send dbus messages
 
-# console level keybinding: when "F5-8" is pressed, switch fo virtual terminal 8, if a programming is running there
+# console level keybinding: when "F5-8" is pressed, switch to virtual terminal 8, if a program is running there
