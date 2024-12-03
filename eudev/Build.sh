2a730c061cb561aaf8a59a16ac926caea08ee942
Damoon Saghian <damoonsaghian84@gmail.com>
2024-12-03 12:33:52 +0330

    Add files via upload

---
+++ eudev/Build.sh
@@ -0,0 +1,8 @@
+project_dir="$(dirname "$0")"
+
+# https://github.com/eudev-project/eudev
+
+# skip eudev-hwids
+# https://pkgs.alpinelinux.org/package/edge/main/x86_64/eudev-hwids
+
+# https://pkgs.alpinelinux.org/package/edge/main/x86_64/eudev-netifnames
