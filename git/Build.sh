41de778d6b4b4ce1ca980cf00d779c22c8a27289
Damoon Saghian <damoonsaghian84@gmail.com>
2024-12-03 12:34:57 +0330

    Add files via upload

---
+++ git/Build.sh
@@ -0,0 +1,7 @@
+project_dir="$(dirname "$0")"
+
+# https://github.com/git/git/blob/master/INSTALL
+# https://github.com/git/git/blob/master/Makefile
+# configure git to use lsh instead of openssh:
+# 	https://github.com/git/git/blob/master/Documentation/config/ssh.txt
+$PKG lsh
