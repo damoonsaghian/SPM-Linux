a52ea6d446f5d7cea38d577e1cc321c65e21ba6a
Damoon Saghian <damoonsaghian84@gmail.com>
2024-12-03 12:50:58 +0330

    Delete sudo/spmbuild.sh

--- sudo/spmbuild.sh
+++
@@ -1,6 +0,0 @@
-project_dir="$(dirname "$0")"
-
-# sudo service: listen to dbus messages
-# sudo client: send dbus messages
-
-# console level keybinding: when "F5-8" is pressed, switch to virtual terminal 8, if a program is running there
