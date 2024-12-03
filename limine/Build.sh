01568969249e31e68b4c4f1e1b293a7815b4056a
Damoon Saghian <damoonsaghian84@gmail.com>
2024-12-03 12:42:44 +0330

    Add files via upload

---
+++ limine/Build.sh
@@ -0,0 +1,5 @@
+project_dir="$(dirname "$0")"
+
+# https://github.com/limine-bootloader/limine
+# https://github.com/limine-bootloader/limine/blob/v8.x/USAGE.md
+# https://github.com/limine-bootloader/limine/blob/v8.x/CONFIG.md
