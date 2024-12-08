f53d786c242c53e769749590698401ee3fec452a
Damoon Saghian <damoonsaghian84@gmail.com>
2024-12-08 23:13:16 +0330

    Add files via upload

--- swapps/Build.sh
+++ swapps/Build.sh
@@ -1,7 +1,6 @@
 project_dir="$(dirname "$0")"
 
-$PKG python-gobject $gnunet_namespace
-$LNK python-gobject *
+spm_include $gnunet_namespace python-gobject
 
 # https://github.com/wmww/gtk4-layer-shell
 
