0ecf0265e0368109e37889ea038298df9dec54bc
Damoon Saghian <damoonsaghian84@gmail.com>
2024-12-18 00:25:14 +0330

    Add files via upload

--- util-linux/login.sh
+++ util-linux/login.sh
@@ -8,7 +8,7 @@ cd /home
 # run services at /home/.spm/exp/sv, as the user 1000 (exas -n)
 
 if [ "$(tty)" = "/dev/tty1" ]; then
-	sudo -n gsh || sudo -n "bash --norc" || sudo -n sh
+	sudo -u1000 gsh || sudo -u1000 "bash --norc" || sudo -u1000 sh
 else
 	bash -l || sh -l
 fi
