56dd978f15f05813a734c77e8d8a8d121b4398be
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-02 09:32:04 +0330

    Add files via upload

--- gstreamer/SPMbuild.sh
+++ gstreamer/SPMbuild.sh
@@ -4,11 +4,10 @@ project_dir="$(dirname "$0")"
 # gst-plugins-base: don't provide xlib
 # gst-plugins-good: don't provide xlib gtk3 libsoup3
 # gst-plugins-ugly: don't provide sidplay
-# gst-plugins-bad: av1(aom-libs), h264(openh264), h265(libde265), and aac(fdk-aac)
+# gst-plugins-bad: av1(aom-libs), h264(openh264), h265(libde265), and aac(faad2)
 # 	don't provide xlib gtk3 directfb dc1394 libopenni openssl curl libneon microdns
 # 	bluez libfreeaptx liblc3 libsbc libldacbt
 # 	fdk-aac vo-aacenc vo-amrwbenc sndfile soundtouch spandsp libwildmidi2 libzxing2
-# 	https://packages.debian.org/sid/gstreamer1.0-plugins-bad
 
 # https://gitlab.freedesktop.org/pipewire/pipewire
 # https://wiki.alpinelinux.org/wiki/PipeWire
