f03f3f1583612d59f62db6f907d721fb5ba93522
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-06 12:50:46 +0330

    Add files via upload

--- bash/SPMbuild.sh
+++ bash/SPMbuild.sh
@@ -2,5 +2,7 @@ project_dir="$(dirname "$0")"
 
 # https://savannah.gnu.org/git/?group=bash
 
-# bin/bash
-# PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> " bash
+cat <<-'EOF' > $build_dir/cmd/bash
+#!/usr/bin/env sh
+PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> " bash
+EOF
