4e15daa1f85457da1b194be4e09ed5562fc4a720
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-06 12:51:45 +0330

    Add files via upload

--- util-linux/login.sh
+++ util-linux/login.sh
@@ -5,6 +5,9 @@ chmod 700 /run/user/1000
 
 cd /home
 
+export HOME="/home"
+export PATH="$PATH:/$HOME/.local/bin"
+
 # run services at /home/.spm/exp/sv, as the user 1000 (exas -n)
 
 if [ "$(tty)" = "/dev/tty1" ]; then
