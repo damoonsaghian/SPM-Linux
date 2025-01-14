4875a479e726a5f09fea4ab481b33cccdac49e85
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-14 23:56:40 +0330

    Add files via upload

--- gnunet/SPMbuild.sh
+++ gnunet/SPMbuild.sh
@@ -17,11 +17,6 @@ OPTIONS = -b -e -a -p
 
 # inhibit suspend/shutdown when a publish is in progress
 
-# https://en.wikipedia.org/wiki/Wireless_mesh_network
-# https://en.wikipedia.org/wiki/WiGig
-
-# decentralized time synchronization using DHT
-
 # gnunet-publish.sh
 # , ask for password
 # , decrypt and namespace mount the egos dir on itself
@@ -33,3 +28,6 @@ OPTIONS = -b -e -a -p
 # generally it's done automatically by the program itself (eg for PostgreSQL)
 # otherwise we must do it manually: chattr +C ... (eg for MariaDB databases)
 # apparently Webkit uses SQLite in WAL mode, but i'm not sure about GnuNet
+
+# for now internally build gnutls and gcrypt
+# in the future replace them with openssl
