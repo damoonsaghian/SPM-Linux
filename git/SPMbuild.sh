339ae13af196520da33933f77bcadfdac2bb1870
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-14 23:30:51 +0330

    Add files via upload

--- git/SPMbuild.sh
+++ git/SPMbuild.sh
@@ -2,6 +2,5 @@ project_dir="$(dirname "$0")"
 
 # https://github.com/git/git/blob/master/INSTALL
 # https://github.com/git/git/blob/master/Makefile
-# configure git to use lsh instead of openssh:
-# 	https://github.com/git/git/blob/master/Documentation/config/ssh.txt
-spm_build $gnunet_namespace lsh
+
+spm_build $gnunet_namespace openssh
