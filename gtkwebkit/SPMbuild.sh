2307d19288d40bbc2b89dd63934de73be84b955c
Damoon Saghian <damoonsaghian84@gmail.com>
2025-01-15 12:14:37 +0330

    Add files via upload

--- gtkwebkit/SPMbuild.sh
+++ gtkwebkit/SPMbuild.sh
@@ -1,3 +1,12 @@
 project_dir="$(dirname "$0")"
 
 # pdfjs
+
+# replace libsoup with curl
+# 	https://lists.webkit.org/pipermail/webkit-help/2009-September/000252.html
+# replace gcrypt with nettle
+# https://blog.cranksoftware.com/webkit-porting-tips-the-good-the-bad-and-the-ugly/
+# https://ariya.io/2011/06/your-webkit-port-is-special-just-like-every-other-port
+# https://trac.webkit.org/wiki/SuccessfulPortHowTo
+# https://trac.webkit.org/wiki/WikiStart
+# https://github.com/WebKit/WebKit/blob/main/Source/cmake/OptionsGTK.cmake
