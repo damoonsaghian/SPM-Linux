2977d6a807e8d1bbe912f1880a1112241752be6c
Damoon Saghian <damoonsaghian84@gmail.com>
2025-02-26 15:47:23 +0330

    Add files via upload

--- gstreamer/SPMbuild.sh
+++ gstreamer/SPMbuild.sh
@@ -8,3 +8,4 @@ project_dir="$(dirname "$0")"
 # 	don't provide xlib gtk3 directfb dc1394 libopenni curl openssl libneon microdns
 # 	bluez libfreeaptx liblc3 libsbc libldacbt flite
 # 	fdk-aac vo-aacenc vo-amrwbenc sndfile soundtouch spandsp libwildmidi2 libzxing2
+# https://github.com/Rafostar/clapper/tree/master/src/lib/gst/plugin
