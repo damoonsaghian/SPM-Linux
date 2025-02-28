a726b09a5e587c827008af0fa763336f2195fd79
Damoon Saghian <damoonsaghian84@gmail.com>
2025-02-28 09:33:07 +0330

    Add files via upload

--- util-linux/SPMbuild.sh
+++ util-linux/SPMbuild.sh
@@ -1,12 +1,8 @@
-project_dir="$(dirname "$0")"
-
 # https://pkgs.alpinelinux.org/package/edge/main/x86_64/util-linux
 # https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/tree/
-# disable: su runuser login flock logger partx setarch cfdisk
+# disable: su runuser login agetty flock logger partx setarch cfdisk
 
-ln "$project_dir"/login.sh "$project_dir"/.cache/spm/exp/cmd/login
-
-# agetty service for each terminal
-# /usr/bin/agetty --skip-login --login-program /exp/cmd/login %I $TERM
+# login executable
+# login service for each terminal
 
 # doas rule for rfkill
--- util-linux/login.sh
+++ util-linux/login.sh
@@ -1,17 +1,29 @@
 #!/exp/cmd/env sh
 
-mkdir -p /run/user/1000
-chmod 700 /run/user/1000
+prepare() {
+	mkdir -p /run/user/1000
+	chmod 700 /run/user/1000
+	
+	cd /home
+	
+	export HOME="/home"
+	export PATH="$PATH:/$HOME/.local/bin"
+	
+	# run services at /home/.spm/exp/sv, as the user 1000 (sudo -u1000 ...)
+}
 
-cd /home
-
-export HOME="/home"
-export PATH="$PATH:/$HOME/.local/bin"
-
-# run services at /home/.spm/exp/sv, as the user 1000 (exas -n)
+# command'line shell
+clsh() {
+	# ask user for lockscreen password
+	# check it (using sudo)
+	# if correct:
+	prepare
+	sudo -u1000 "bash --norc" || sudo -u1000 sh
+}
 
 if [ "$(tty)" = "/dev/tty1" ]; then
-	sudo -u1000 gsh || sudo -u1000 "bash --norc" || sudo -u1000 sh
+	prepare
+	sudo -u1000 gsh || clsh
 else
-	sudo -u1000 "bash --norc" || sudo -u1000 sh
+	clsh
 fi
