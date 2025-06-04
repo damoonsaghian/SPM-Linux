35c96dc2a07cce7d5437e1898dba12dada5de5ad
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-04 14:28:42 +0330

    Add files via upload

---
+++ swapp/main.cc
@@ -0,0 +1,48 @@
+// https://github.com/qt/qtwayland
+// https://doc.qt.io/qt-6/qtwaylandcompositor-index.html
+// https://gitlab.com/desktop-frameworks/wayqt/
+// https://github.com/lirios/shell
+
+// https://docs.voidlinux.org/config/session-management.html
+// https://git.sr.ht/~kennylevinsen/seatd
+
+// apps will open in separate desktops
+// extra windows will be floating with border shadow, and will be closed when unfocused
+
+/*
+lock
+on statusbar replace apps with a lock icon, that shows the password prompt when clicked on
+super, alt+tab, alt+space in lock mode: switch run codev lock, switch to codev workspace and close non'codev windows
+password prompt closes (showing codev in lock mode) when Escape is pressed,
+	or when empty password is entered, or simply when password prompt is unfocused
+start in locked mode
+to prevent BadUSB, lock when a new input device is connected
+*/
+
+// keybinding to show the launcher
+// release Super_L or Super_R
+// Alt+space
+
+// keybinding to switch between open apps
+// Super+space
+// Alt+Tab
+
+//keybinding to close window
+// Super+Backspace
+// Super+Escape
+// Alt+Backspace
+// Alt+Escape
+
+// after 600 seconds idle: lock, turn screen off
+// dim screen in several steps before turning screen off
+
+// screenshot and screencast
+// https://github.com/ammen99/wf-recorder
+// https://gitlab.freedesktop.org/emersion/grim
+
+// hide cursor after 8 seconds, and when typing begins
+
+// on'screen keyboard
+// https://github.com/qt/qtvirtualkeyboard
+
+// voice control
