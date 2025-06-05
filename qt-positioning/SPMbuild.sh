a1194f94edb8d59af2ab5e4722f76e9734e24b8b
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-06 00:00:30 +0330

    Add files via upload

--- qt-positioning/SPMbuild.sh
+++ qt-positioning/SPMbuild.sh
@@ -1,13 +1,12 @@
-project_dir="$(dirname "$0")"
+# https://github.com/qt/qtpositioning
+# https://doc.qt.io/archives/qt-5.15/qtpositioning-index.html
 
-# modemmanager A-GPS
-# assisted data can be downloaded from SUPL servers: supl.google.com with 7276 or 7275 port
-# then injected into the GPS device
-# https://www.freedesktop.org/software/ModemManager/api/latest/gdbus-org.freedesktop.ModemManager1.Modem.Location.html#gdbus-method-org-freedesktop-ModemManager1-Modem-Location.InjectAssistanceData
+# https://archlinux.org/packages/extra/x86_64/qt6-positioning/
+# https://archlinux.org/packages/extra/x86_64/qt6-serialport/
 
-# use gpsd for standalone gps devices (without cellular)
-# https://gpsd.gitlab.io/gpsd/index.html
-# gspd does not support injecting assisted data, yet
-# actually i don't know if there is any standalone gps device with this ability
-
-# https://wiki.archlinux.org/title/GPS
+# if target is Posix, build and install a Geoclue service provider that uses Ofono to obtain position
+# https://www.freedesktop.org/software/geoclue/docs/
+# https://gitlab.freedesktop.org/geoclue/geoclue/-/tree/master/src
+# https://github.com/mer-hybris/geoclue-providers-mlsdb/blob/master/plugin/mlsdbprovider.cpp
+# https://github.com/mer-hybris/geoclue-providers-hybris/blob/master/hybrisprovider.cpp
+# https://git.kernel.org/pub/scm/network/ofono/ofono.git/tree/doc/location-reporting-api.txt
