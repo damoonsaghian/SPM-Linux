6264f9b2a4090fe61a4e86b9d6103298d492a8b5
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-06 00:09:16 +0330

    Add files via upload

--- qt/SPMbuild.sh
+++ qt/SPMbuild.sh
@@ -1,6 +1,25 @@
-project_dir="$(dirname "$0")"
+# https://github.com/qt/qtbase
+# https://github.com/qt/qtbase/blob/dev/config_help.txt
+# cmake args: -DFEATURE_glib=OFF -DFEATURE_xcb_xlib=OFF
 
-# libjxl libavif webp-pixbuf-loader librsvg
-# hardlink the gdk-pixbuf loaders
+# skip Qt Network
+# if WIN32, build a limited version of Qt Network but without ssl
+# because it's apparently used as an replacement of DBus, for IPC
 
-# scroll.css
+# use lines on borders of scrolled widgets to show the amount of overflowed content
+
+# https://github.com/qt/qtimageformats
+# libjxl libavif webp librsvg
+
+# terminal emulator
+# https://github.com/KDE/qmlkonsole
+# https://github.com/contour-terminal/contour
+# https://github.com/sailfishos/fingerterm
+# https://github.com/Swordfish90/cool-retro-term
+# https://github.com/lirios/terminal
+# https://github.com/rburchell/literm
+
+# OpenStreetMap viewer
+# https://github.com/qt/qtlocation
+
+# https://doc.qt.io/qt-6/qtpdf-index.html
