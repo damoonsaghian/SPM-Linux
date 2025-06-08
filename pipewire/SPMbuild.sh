c31e087c06a888a32abf9a6a24e1610eb984c6d9
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-08 23:26:34 +0330

    Add files via upload

--- pipewire/SPMbuild.sh
+++ pipewire/SPMbuild.sh
@@ -1,16 +1,12 @@
-project_dir="$(dirname "$0")"
-
 # https://gitlab.freedesktop.org/pipewire/pipewire
 # https://wiki.alpinelinux.org/wiki/PipeWire
 # https://docs.voidlinux.org/config/media/pipewire.html
 # https://wiki.archlinux.org/title/PipeWire
 
-# gst-plugin-pipewire
-# pipewire-pulse
-# pipewire-spa-bluez
+# https://gitlab.freedesktop.org/pipewire/media-session
 
-mkdir -p etc/wireplumber/main.lua.d
-echo 'device_defaults.properties = {
-	["default-volume"] = 1.0,
-	["default-input-volume"] = 1.0,
-}' > etc/wireplumber/main.lua.d/51-default-volume.lua
+# without gstreamer glib pulse jack
+
+# libpw-v4l2.so
+
+# pipewire-spa-bluez
