32bf5a8e3dcee6419f4ffc6723c7269c2f0dcd57
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-08 23:15:51 +0330

    Add files via upload

--- clang/SPMbuild.sh
+++ clang/SPMbuild.sh
@@ -1,3 +1,10 @@
-project_dir="$(dirname "$0")"
+spm_import $gnunet_namespace llvm
+
+cd "$pkg_dir/.cache/git"
+gitag_clone
+
+mkdir "$pkg_dir/.cache/build/$ARCH"
+cd "$pkg_dir/.cache/build/$ARCH"
+# build the project in "$pkg_dir/.cache/git/"
 
 # https://mcilloni.ovh/2021/02/09/cxx-cross-clang/
