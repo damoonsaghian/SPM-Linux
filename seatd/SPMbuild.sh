abce0b6088925d7b218197675f192a16670f16e5
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-08 23:33:22 +0330

    Delete seatd/SPMbuild.sh

--- seatd/SPMbuild.sh
+++
@@ -1,6 +0,0 @@
-project_dir="$(dirname "$0")"
-
-# https://docs.voidlinux.org/config/session-management.html
-# https://git.sr.ht/~kennylevinsen/seatd
-# https://docs.voidlinux.org/config/power-management.html
-# https://pkgs.alpinelinux.org/package/edge/main/x86_64/acpid
