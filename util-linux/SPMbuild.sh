6f9f21a1d608f03ad69c46353f43db06e5742754
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-25 20:42:34 +0330

    Add files via upload

--- util-linux/SPMbuild.sh
+++ util-linux/SPMbuild.sh
@@ -3,7 +3,5 @@
 # https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/tree/meson_options.txt
 # mount umount libmount mountpoint lsblk blkid libblkid uuidgen libuuid blkdiscard blkzone blockdev sfdisk fdisk libfdisk
 # findfs findmnt mkswap swapon swapoff eject losetup fallocate wipefs flock fstrim
-# dmesg setpriv pivot_root switch_root unshare hwclock
-# rfkill taskset
-# renice rtcwake
+# dmesg setpriv pivot_root switch_root unshare hwclock rfkill renice taskset rtcwake
 # disable: su runuser login agetty mcookie logger partx cfdisk setarch lscpu lsmem ...
