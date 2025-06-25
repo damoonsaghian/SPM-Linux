f1e15a7f5decadd6f1658db45cdde333c48dc9f7
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-25 15:34:28 +0330

    Delete util-linux directory

--- util-linux/SPMbuild.sh
+++
@@ -1,8 +0,0 @@
-# https://pkgs.alpinelinux.org/package/edge/main/x86_64/util-linux
-# https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/tree/
-# disable: su runuser login agetty flock logger partx setarch cfdisk
-
-# login executable
-# login service for each terminal
-
-# sudo rule for rfkill
