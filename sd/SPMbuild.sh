83f6dcf41935538993c73206517f3b679b9d6a98
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-25 19:14:17 +0330

    Add files via upload

--- sd/SPMbuild.sh
+++ sd/SPMbuild.sh
@@ -6,3 +6,5 @@
 # https://git.kernel.org/pub/scm/linux/kernel/git/kdave/btrfs-progs.git/about/
 # https://github.com/dosfstools/dosfstools
 # https://github.com/exfatprogs/exfatprogs
+
+# https://github.com/netheril96/securefs
--- sd/sd.cc
+++ sd/sd.cc
@@ -3,10 +3,20 @@ mounting and formatting storage devices
 
 list of devices, notified when changed
 https://api-staging.kde.org/solid-index.html
+	disable battery and processor
 https://gitlab.com/desktop-frameworks/storage
 
 mount with suid bits disabled
 mount to ~/.local/state/mounts
+
+mount with read-write access only for processes running with a specific group id
+sd mount <group-id>
+
+creating and mounting encrypted directories
+https://github.com/netheril96/securefs
+https://www.agwa.name/blog/post/easily_running_fuse_in_an_isolated_mount_namespace
+sd encrypt <dir-path>
+sd decrypt <dir-path>
 
 to access the content of mtp devices:
 https://sourceforge.net/p/libmtp
@@ -23,4 +33,7 @@ exit if it's the system device
 format devices
 type: fat
 mkfs-args: -F, 32, -I (to override partitions)
+format non'system devices, format with vfat or exfat (if wants files bigger than 4GB)
+for system devices:
+sudo sh -c "mkfs.btrfs -f <dev-path>; mount <dev-path> /mnt; chmod 777 /mnt; umount /mnt"
 /*
