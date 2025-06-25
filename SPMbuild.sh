1d5555680c0719410603ce12359f3a0c9cf56fae
Damoon Saghian <damoonsaghian84@gmail.com>
2025-06-25 17:23:07 +0330

    Add files via upload

---
+++ SPMbuild.sh
@@ -0,0 +1,9 @@
+# https://pkgs.alpinelinux.org/package/edge/main/x86_64/util-linux
+# https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/tree/
+# https://git.kernel.org/pub/scm/utils/util-linux/util-linux.git/tree/meson_options.txt
+# mount umount libmount mountpoint lsblk blkid libblkid uuidgen libuuid blkdiscard blkzone blockdev sfdisk fdisk libfdisk
+# findfs findmnt mkswap swapon swapoff eject losetup fallocate wipefs flock fstrim
+# dmesg setpriv pivot_root switch_root unshare hwclock
+# rfkill taskset
+# renice rtcwake
+# disable: su runuser login agetty mcookie logger partx cfdisk setarch lscpu lsmem ...
