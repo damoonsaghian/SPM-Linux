c2472ac4606fc02fc9b460a93edd27db1cf5399f
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:28:54 +0330

    Add files via upload

--- eudev/SPMbuild.sh
+++ eudev/SPMbuild.sh
@@ -1,8 +1,9 @@
-project_dir="$(dirname "$0")"
-
 # https://github.com/eudev-project/eudev
 
 # skip eudev-hwids
 # https://pkgs.alpinelinux.org/package/edge/main/x86_64/eudev-hwids
 
 # https://pkgs.alpinelinux.org/package/edge/main/x86_64/eudev-netifnames
+
+# files in /dev/input can be read by users that are in the group with gid 1
+# files in /dev/dri can be written to by users that are in the group with gid 2
