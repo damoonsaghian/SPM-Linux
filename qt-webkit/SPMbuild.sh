7c3e7badddc4adff688e525055c6a863049389a5
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:51:15 +0330

    Add files via upload

--- qt-webkit/SPMbuild.sh
+++ qt-webkit/SPMbuild.sh
@@ -1,6 +1,10 @@
-# https://github.com/qtwebkit/qtwebkit/issues/880
+# https://github.com/movableink/webkit
+# https://github.com/movableink/webkit/blob/master/Source/cmake/OptionsQt.cmake
 
-# create a webkit port with qt, http, nettle
+# https://github.com/qt/qtwebchannel
+
+# link gcrypt statcally
+# or replace it with nettle:
 # https://blog.cranksoftware.com/webkit-porting-tips-the-good-the-bad-and-the-ugly/
 # https://ariya.io/2011/06/your-webkit-port-is-special-just-like-every-other-port
 # https://trac.webkit.org/wiki/SuccessfulPortHowTo
