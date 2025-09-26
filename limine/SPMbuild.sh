86b6f96b5662fc614b605f7ecfef8414eb5688f3
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:34:35 +0330

    Add files via upload

--- limine/SPMbuild.sh
+++ limine/SPMbuild.sh
@@ -1,5 +1,3 @@
-project_dir="$(dirname "$0")"
-
 # https://github.com/limine-bootloader/limine
 # https://github.com/limine-bootloader/limine/blob/v8.x/USAGE.md
 # https://github.com/limine-bootloader/limine/blob/v8.x/CONFIG.md
