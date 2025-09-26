d2cb3dc94630a6b80b7777aabe722e27c0a82b61
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:30:42 +0330

    Add files via upload

--- fontconfig/SPMbuild.sh
+++ fontconfig/SPMbuild.sh
@@ -7,33 +7,7 @@
 # , large punctuation
 # , and easily distinguishable characters
 # , while allowing each character to take up the space that it needs
-# "https://github.com/iaolo/iA-Fonts/tree/master/iA%20Writer%20Quattro"
-# "https://input.djr.com/"
+# Iosevka Aile (just change "I" character)
+# "https://github.com/iaolo/iA-Fonts/tree/master/iA%20Writer%20Quattro" (just change "I" character)
 
-# fonts-noto-core fonts-hack
-
-mkdir -p /etc/fonts
-echo -n '<?xml version="1.0"?>
-<!DOCTYPE fontconfig SYSTEM "fonts.dtd">
-<fontconfig>
-	<selectfont>
-		<rejectfont>
-			<pattern><patelt name="family"><string>NotoNastaliqUrdu</string></patelt></pattern>
-			<pattern><patelt name="family"><string>NotoKufiArabic</string></patelt></pattern>
-			<pattern><patelt name="family"><string>NotoNaskhArabic</string></patelt></pattern>
-		</rejectfont>
-	</selectfont>
-	<alias>
-		<family>serif</family>
-		<prefer><family>NotoSerif</family></prefer>
-	</alias>
-	<alias>
-		<family>sans</family>
-		<prefer><family>NotoSans</family></prefer>
-	</alias>
-	<alias>
-		<family>monospace</family>
-		<prefer><family>Hack</family></prefer>
-	</alias>
-</fontconfig>
-' > /etc/fonts/local.conf
+# noto fonts for east and middle eastern characters
