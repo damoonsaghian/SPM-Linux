0e9a1e61a796207fc6fcde44c6bbf4a7b87f3b2f
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:21:09 +0330

    Add files via upload

--- avahi/SPMbuild.sh
+++ avahi/SPMbuild.sh
@@ -1,10 +1,1 @@
-project_dir="$(dirname "$0")"
-
-# https://savannah.gnu.org/git/?group=bash
-
-cat <<-'EOF' > $build_dir/cmd/bash
-#!/usr/bin/env sh
-PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> " bash
-EOF
-
-# do not load ~/.bashrc and ~/bash_profile and ~/.profile
+# skip avahi-glib
