95fe5fc613069689bfa7a1295181417e0d82e6b4
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 23:07:48 +0330

    Add files via upload

--- sh/SPMbuild.sh
+++ sh/SPMbuild.sh
@@ -1,3 +1,7 @@
-project_dir="$(dirname "$0")"
-
+# dash (as "sh" command to run system scripts)
 # https://git.kernel.org/pub/scm/utils/dash/dash.git/tree/
+
+# bash (for interactive shell)
+# https://cgit.git.savannah.gnu.org/cgit/bash.git/tree/README
+# PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> "
+# do not load ~/.bashrc and ~/bash_profile and ~/.profile
