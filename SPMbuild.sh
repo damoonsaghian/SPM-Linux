777884357a5f37729a48d26a41c7bfe977923b89
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:58:31 +0330

    Add files via upload

---
+++ SPMbuild.sh
@@ -0,0 +1,3 @@
+# https://gitlab.freedesktop.org/poppler/poppler
+# https://gitlab.freedesktop.org/poppler/poppler/-/blob/master/CMakeLists.txt
+# ENABLE_NSS3=OFF ENABLE_GPGME=OFF ENABLE_QT5=OFF ENABLE_GLIB=OFF
