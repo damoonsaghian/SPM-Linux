7aa2a499905f3489eaa5a486608bc22d78f8b0af
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:26:14 +0330

    Add files via upload

--- curl/SPMbuild.sh
+++ curl/SPMbuild.sh
@@ -1,6 +1,5 @@
-# gnutls backend
+# http/https only curl (with gnutls backend)
 
-# only build http/https protocols
 # for http/3:
 # https://github.com/ngtcp2/ngtcp2
 # https://github.com/lxin/quic (only works on linux)
