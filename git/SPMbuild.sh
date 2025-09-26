c21810d626dffea2ca6a39add83c7ecfde21b180
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:31:18 +0330

    Add files via upload

--- git/SPMbuild.sh
+++ git/SPMbuild.sh
@@ -1,5 +1,2 @@
 # https://github.com/git/git/blob/master/INSTALL
 # https://github.com/git/git/blob/master/Makefile
-
-# configure git to use ssh program in a way that is compatible with lsh:
-# https://github.com/git/git/blob/master/Documentation/config/ssh.adoc
