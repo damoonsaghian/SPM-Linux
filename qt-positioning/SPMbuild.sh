6dc02d2dc956cb7073c18875baade86bda79ce34
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:47:13 +0330

    Add files via upload

--- qt-positioning/SPMbuild.sh
+++ qt-positioning/SPMbuild.sh
@@ -1,12 +1,3 @@
+spm_import qt-serialport
+
 # https://github.com/qt/qtpositioning
-# https://doc.qt.io/archives/qt-5.15/qtpositioning-index.html
-
-# https://archlinux.org/packages/extra/x86_64/qt6-positioning/
-# https://archlinux.org/packages/extra/x86_64/qt6-serialport/
-
-# if target is Posix, build and install a Geoclue service provider that uses Ofono to obtain position
-# https://www.freedesktop.org/software/geoclue/docs/
-# https://gitlab.freedesktop.org/geoclue/geoclue/-/tree/master/src
-# https://github.com/mer-hybris/geoclue-providers-mlsdb/blob/master/plugin/mlsdbprovider.cpp
-# https://github.com/mer-hybris/geoclue-providers-hybris/blob/master/hybrisprovider.cpp
-# https://git.kernel.org/pub/scm/network/ofono/ofono.git/tree/doc/location-reporting-api.txt
