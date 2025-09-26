0c440680b8c112afabd1ad69ba81f4662b4466c1
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-26 22:31:36 +0330

    Add files via upload

--- gnunet/SPMbuild.sh
+++ gnunet/SPMbuild.sh
@@ -1,18 +1,6 @@
 # https://git.gnunet.org/gnunet.git/tree/src
 
-# https://docs.gnunet.org/latest/users/subsystems.html
-# https://docs.gnunet.org/latest/users/configuration.html#access-control-for-gnunet
-# https://manpages.debian.org/unstable/gnunet/gnunet.1.en.html
-# https://manpages.debian.org/unstable/gnunet/index.html
-# https://wiki.archlinux.org/title/GNUnet
-
-echo '[ats]
-WLAN_QUOTA_IN = unlimited
-WLAN_QUOTA_OUT = unlimited
-WAN_QUOTA_IN = unlimited
-WAN_QUOTA_OUT = unlimited
-
-[hostlist]
+echo '[hostlist]
 # Options:
 # -p : provide a hostlist as a hostlist servers
 # -b : bootstrap using configured hostlist servers
@@ -21,19 +9,17 @@ WAN_QUOTA_OUT = unlimited
 OPTIONS = -b -e -a -p
 ' > "$project_dir/.cache/spm/gnunet.conf"
 
-# gn-publish.sh
-# gn-download.sh
+# intresting fact: gnunet uses UDP to discover peers on local net
 
-# only make system services (do not create system services for normal user)
+# https://docs.gnunet.org/latest/users/configuration.html#limitations-and-known-bugs
+# https://docs.gnunet.org/latest/users/subsystems.html#transport-ng-next-generation-transport-management
+# https://en.wikipedia.org/wiki/Long-range_Wi-Fi
 
-# for database files on BTRFS, COW must be disabled
-# generally it's done automatically by the program itself (eg for PostgreSQL)
-# otherwise we must do it manually: chattr +C ... (eg for MariaDB databases)
-# apparently Webkit uses SQLite in WAL mode, but i'm not sure about GnuNet database
-
-# wifi ad hoc
-# https://docs.gnunet.org/latest/users/configuration.html#configuring-the-wlan-transport-plugin
+# LoRa communicator for emergency communications (when normal network infrastructure is down)
+# https://en.wikipedia.org/wiki/LoRa
+# actually a separate LoRa device can by itself be useful in emergency situations
+# 	normally it should have a rechargable battery, and a manual switch too
 
 # for now, build libsodium and gcrypt internally, and link statically
-# in the future, replace it with nettle
-# and for more security add NTRU on top (like in openssh)
+# it will be good if GNUnet replaces them with nettle
+# and even add NTRU on top, for more security (like in openssh)
