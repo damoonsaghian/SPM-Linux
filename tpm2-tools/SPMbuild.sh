a886567c773ce3bd186a205ac04f7f3286e6adc4
Damoon Saghian <damoonsaghian84@gmail.com>
2025-09-28 23:52:03 +0330

    Update and rename SPMbuild.sh to SPMbuild.sh

--- tpm2-tools/SPMbuild.sh
+++ tpm2-tools/SPMbuild.sh
@@ -1,4 +1,4 @@
-# https://github.com/tpm2-software/tpm2-tss
+# https://github.com/tpm2-software/tpm2-tools
 
 # gnutls implementes a compatibility wrapper for openssl; try to use that
 # if it failed, use openssl
