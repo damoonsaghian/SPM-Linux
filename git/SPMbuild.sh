ee5b7cda8a1126276f2ca2102a5caaf50e54cf7d
Damoon Saghian <damoonsaghian84@gmail.com>
2025-10-07 11:45:47 +0330

    Add files via upload

--- git/SPMbuild.sh
+++ git/SPMbuild.sh
@@ -1,2 +1,6 @@
 # https://github.com/git/git/blob/master/INSTALL
 # https://github.com/git/git/blob/master/Makefile
+
+# git clones are done in /var/cache/git/clones/
+# the dir is writable only for users in group 11
+# git has its GUID set for group 11
