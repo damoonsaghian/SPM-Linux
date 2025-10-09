52cc7dee67c731d2c06af25599fe08689e7e873d
Damoon Saghian <damoonsaghian84@gmail.com>
2025-10-09 22:47:24 +0330

    Add files via upload

--- sh/SPMbuild.sh
+++ sh/SPMbuild.sh
@@ -1,7 +1,9 @@
 # dash (as "sh" command to run system scripts)
 # https://git.kernel.org/pub/scm/utils/dash/dash.git/tree/
 
-# bash (for interactive shell)
-# https://cgit.git.savannah.gnu.org/cgit/bash.git/tree/README
-# PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> "
-# do not load ~/.bashrc and ~/bash_profile and ~/.profile
+# zsh (for interactive shell)
+# https://sourceforge.net/p/zsh/code/ci/master/tree/
+printf '
+PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> "
+' > "$spm_share"/zsh/.zshrc
+ln "script_dir"/zsh.zprofile "$spm_share"/zsh/.zprofile
