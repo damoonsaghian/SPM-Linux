0ca70fc40b5f19b272095c5d70abcbce3b770e22
Damoon Saghian <damoonsaghian84@gmail.com>
2025-10-12 14:02:37 +0330

    Add files via upload

--- sh/SPMbuild.sh
+++ sh/SPMbuild.sh
@@ -3,7 +3,3 @@
 
 # zsh (for interactive shell)
 # https://sourceforge.net/p/zsh/code/ci/master/tree/
-printf '
-PS1="\e[7m \u@\h \e[0m \e[7m \w \e[0m\n> "
-' > "$spm_share"/zsh/.zshrc
-ln "script_dir"/zsh.zprofile "$spm_share"/zsh/.zprofile
