ae336319a92703dab06c7541364a7caa80610f96
Damoon Saghian <damoonsaghian84@gmail.com>
2025-10-18 09:27:52 +0330

    Add files via upload

--- sh/SPMbuild.sh
+++ sh/SPMbuild.sh
@@ -1,5 +1,5 @@
 # dash (as "sh" command to run system scripts)
 # https://git.kernel.org/pub/scm/utils/dash/dash.git/tree/
 
-# zsh (for interactive shell)
-# https://sourceforge.net/p/zsh/code/ci/master/tree/
+# bash (for interactive shell)
+# https://cgit.git.savannah.gnu.org/cgit/bash.git
