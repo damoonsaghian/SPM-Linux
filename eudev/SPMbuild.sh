7a6adde00e6c352a7b33da2cc0d3f91fb7f5aa41
Damoon Saghian <damoonsaghian84@gmail.com>
2025-10-19 11:30:43 +0330

    Add files via upload

--- eudev/SPMbuild.sh
+++ eudev/SPMbuild.sh
@@ -8,3 +8,7 @@
 # when creating /dev/input devices, set their group to 1
 # when creating /dev/dri devices, set their group to 2
 # when creating /dev/snd/* and /dev/video* devices, set their group to 1000
+
+# to prevent BadUSB, cerate evdev rule that when a new input device is connected:
+# touch /tmp/lock-bash
+# chown 1000 /tmp/lock-bash
