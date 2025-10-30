375c195d0f1d04609a7a061c17fa31fbf4fe6611
Damoon Saghian <damoonsaghian84@gmail.com>
2025-10-30 23:48:44 +0330

    Add files via upload

--- fontconfig/SPMbuild.sh
+++ fontconfig/SPMbuild.sh
@@ -1,7 +1,7 @@
 # mono'space fonts:
 # , wide characters are forced to squeeze
 # , narrow characters are forced to stretch
-# , bold characters don’t have enough room
+# , bold characters don't have enough room
 # proportional font for code:
 # , generous spacing
 # , large punctuation
@@ -10,8 +10,8 @@
 # Iosevka Aile (just change "I" character)
 # "https://github.com/iaolo/iA-Fonts/tree/master/iA%20Writer%20Quattro" (just change "I" character)
 
+# monospace font is still needed for terminal emulator
+# https://github.com/adobe-fonts/source-code-pro
+
 # noto fonts for: math, symbols, emoji, armenian georgian hebrew arabic ethiopic nko,
 # 	devanagari gujarati telugu kannada malayalam oriya bengali tamil myanmar thai lao khmer cjk
-
-# for terminal:
-# https://github.com/eigilnikolajsen/commit-mono
