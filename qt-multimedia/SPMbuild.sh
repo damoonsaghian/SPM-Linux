b04fe2a3f5ee4681e261a4c7c481b74537d60364
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-05 11:57:53 +0330

    Delete qt-multimedia directory

--- qt-multimedia/SPMbuild.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/qt/qtmultimedia/tree/dev
-
-# ffmpeg backend https://github.com/qt/qtmultimedia/blob/dev/src/plugins/multimedia/ffmpeg/CMakeLists.txt
-# 	disable QT_FEATURE_xlib
-# https://github.com/qt/qtmultimedia/blob/dev/src/multimedia/CMakeLists.txt
-# enable alsa, disable pulse
