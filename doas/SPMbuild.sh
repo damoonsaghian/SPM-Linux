7596311509efad6e569eb6a045dd689b3d40e03c
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-05 12:00:26 +0330

    Delete doas directory

--- doas/SPMbuild.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/Duncaen/OpenDoas
-
-# https://manpages.debian.org/trixie/opendoas/doas.conf.5.en.html
-# setenv TZ
-
-# doas rule for sudo
--- doas/sudo.sh
+++
@@ -1,8 +0,0 @@
-[ "$1" = "--passwd" ] && {
-	root_dir="$2"
-	# read password
-	# store it in "$root_dir"/var/lib/passwd hashed, without read permission for any user other than root
-	exit
-}
-
-# ask for password, and if correct exec "$1"
