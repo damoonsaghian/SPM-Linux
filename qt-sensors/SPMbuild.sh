80b02bb65689d52ea819fde2bc84ff256b2de899
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-05 11:57:35 +0330

    Delete qt-sensors directory

--- qt-sensors/SPMbuild.sh
+++
@@ -1,4 +0,0 @@
-# https://github.com/qt/qtsensors
-# https://github.com/qt/qtsensors/tree/dev/src/plugins/sensors/sensorfw
-# https://github.com/sailfishos/sensorfw
-# https://github.com/sailfishos/sensorfw/blob/master/doc/PLUGIN-GUIDE
