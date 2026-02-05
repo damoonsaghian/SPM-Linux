2b26fbef52a8b194e4dd4affa0cf886867f6ceed
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-05 12:00:12 +0330

    Delete git directory

--- git/SPMbuild.sh
+++
@@ -1,6 +0,0 @@
-# https://github.com/git/git/blob/master/INSTALL
-# https://github.com/git/git/blob/master/Makefile
-
-# git clones are done in /var/cache/git/clones/
-# the dir is writable only for users in group 11
-# git has its GUID set for group 11
