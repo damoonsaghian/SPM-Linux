a843b8c60bd21be23504c42400c440abb7ea516e
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-05 11:57:51 +0330

    Delete qt-poppler directory

--- qt-poppler/SPMbuild.sh
+++
@@ -1,3 +0,0 @@
-# https://gitlab.freedesktop.org/poppler/poppler
-# https://gitlab.freedesktop.org/poppler/poppler/-/blob/master/CMakeLists.txt
-# ENABLE_NSS3=OFF ENABLE_GPGME=OFF ENABLE_QT5=OFF ENABLE_GLIB=OFF
