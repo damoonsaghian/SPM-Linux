10781e61e33c1b1c525d1c50c554294f9e4b5c7f
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-05 11:55:52 +0330

    Delete sh directory

--- sh/SPMbuild.sh
+++
@@ -1,5 +0,0 @@
-# dash (as "sh" command to run system scripts)
-# https://git.kernel.org/pub/scm/utils/dash/dash.git/tree/
-
-# bash (for interactive shell)
-# https://cgit.git.savannah.gnu.org/cgit/bash.git
