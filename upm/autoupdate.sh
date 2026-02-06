5c10bbf5984ee8199861dd5ac634cdebc5c0f996
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-06 16:07:45 +0330

    Add files via upload

--- upm/autoupdate.sh
+++ upm/autoupdate.sh
@@ -1,12 +1,16 @@
-metered_connection() {
-	#nmcli --terse --fields GENERAL.METERED dev show | grep --quiet "yes"
-	#dbus: org.freedesktop.NetworkManager Metered
-}
+#!/usr/bin/env sh
 
-metered_connection && exit 0
+# a 5min delay, for when it's started on boot
+sleep 300
 
-# if plugged
+# do not run autoupdate on metered connection
+case nmcli --terse --field METERED general in
+yes*) exit 11 ;;
+esac
 
-# inhibit suspend/shutdown when an upgrade is in progress
+[ -e /sys/class/power_supply/BAT0 ] &&
+	[ "$(cat /sys/class/power_supply/BAT0/status)" = Discharging ] &&
+	[ "$(cat /sys/class/power_supply/BAT0/capacity)" -lt 10 ] &&
+	exit 11
 
-# if during autoupdate an error occures: echo error > /var/cache/autoupdate-status
+upm update || echo error > /tmp/upm-status
