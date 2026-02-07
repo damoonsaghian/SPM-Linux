8df15497bf740bb1829bea4e8f0aa0b065fc1163
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-07 22:18:22 +0330

    Add files via upload

--- Ubuild.sh
+++ Ubuild.sh
@@ -515,6 +515,18 @@ pkg=tpm2-tools
 # gnutls implementes a compatibility wrapper for openssl; try to use that
 # if it failed, use openssl
 
+pkg=uni
+
+upm_import qt
+upm_import quickshell # needed for Process qml type
+upm_import curl
+upm_import mauikit-filebrowsing
+upm_import archive
+
+upm_xcript inst/app uni
+
+ln "$pkg_dir/.data/uni.svg" "$pkg_dir/.cache/upm/$ARCH/app/uni.svg"
+
 pkg=upm
 
 upm_import git
