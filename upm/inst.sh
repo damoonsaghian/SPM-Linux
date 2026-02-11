b265744320b2b4348243f3b3b90b2c3fb2c3da52
Damoon Saghian <damoonsaghian84@gmail.com>
2026-02-11 11:17:28 +0330

    Add files via upload

--- upm/inst.sh
+++ upm/inst.sh
@@ -5,7 +5,10 @@ gnunet_namespace="$(cat "$(dirname "$(readlink -f "$0")")"/../.meta/gns)"
 state_dir="$XDG_STATE_HOME"
 [ -z "$state_dir" ] && state_dir="$HOME/.local/state"
 
-if [ "$1" = src ]; then
+echo "UPM will try to download binary packages (instead of building from source), if they are available for your system"
+printf "do you want to always built packages from source? (y/N) "
+read -r ans
+if [ "$ans" = y ]; then
 	mkdir -p "$state_dir"/spm
 	echo "build'from'src" > "$state_dir"/spm/config
 fi
@@ -14,4 +17,3 @@ spm_dir="$state_dir/spm/builds/$gnunet_namespace/spm"
 mkdir -p "$spm_dir"
 cp "$(dirname "$0")/spm.sh" "$spm_dir/"
 sh "$spm_dir"/spm.sh install "$gnunet_namespace" spm
-exit
